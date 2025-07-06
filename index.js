b8f9b2944ce468c20487a4550e6cb44457ca332b
aman-deep940 <amandeep.vdoit@gmail.com>
2025-07-06 16:10:09 +0530

    added websocket

---
+++ index.js
@@ -0,0 +1,115 @@
+import { WebSocket, WebSocketServer } from "ws";
+import jwt from "jsonwebtoken";
+const JWT_SECRET = "123212321";
+import { prismaClient } from "./prisma.js";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+const users = [];
+
+function checkUser(token) {
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    if (typeof decoded == "string") {
+      return null;
+    }
+
+    if (!decoded || !decoded.userId) {
+      return null;
+    }
+
+    return decoded.userId;
+  } catch (e) {
+    return null;
+  }
+}
+
+wss.on("connection", function connection(ws, request) {
+  const url = request.url;
+
+  if (!url) {
+    return;
+  }
+
+  const queryParams = new URLSearchParams(url.split("?")[1]);
+
+  const token = queryParams.get("token") || "";
+
+  const userId = checkUser(token);
+
+  if (userId == null) {
+    ws.close();
+    return null;
+  }
+
+  users.push({
+    userId,
+    rooms: [],
+    ws,
+  });
+
+  ws.on("message", async function message(data) {
+    let parsedData;
+    if (typeof data !== "string") {
+      parsedData = JSON.parse(data.toString());
+    } else {
+      parsedData = JSON.parse(data);
+    }
+
+    if (parsedData.type === "join_room") {
+      const user = users.find((x) => x.ws === ws);
+      user?.rooms.push(parsedData.roomId);
+    }
+
+    if (parsedData.type == "leave_room") {
+      const user = users.find((x) => x.ws === ws);
+
+      if (!user) {
+        return;
+      }
+
+      user.rooms = user?.rooms.filter((x) => x !== parsedData.roomId);
+    }
+
+    if (parsedData.type === "chat") {
+      const roomId = parsedData.roomId;
+      const message = parsedData.message;
+
+      if (parsedData.message.shape?.type === "eraser") {
+        users.forEach((user) => {
+          if (user.rooms.includes(roomId)) {
+            user.ws.send(
+              JSON.stringify({
+                type: "chat",
+                message: message,
+                roomId,
+              })
+            );
+          }
+        });
+        return;
+      }
+
+      await prismaClient.chat.create({
+        data: {
+          roomId: Number(roomId),
+          message,
+          userId,
+        },
+      });
+
+      users.forEach((user) => {
+        if (user.rooms.includes(roomId)) {
+          user.ws.send(
+            JSON.stringify({
+              type: "chat",
+              message: message,
+              roomId,
+            })
+          );
+        }
+      });
+    }
+  });
+});
